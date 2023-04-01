5a345d542cff71af01c31b1327dfae0cd8e631d6
phredmund <eexley@gmail.com>
2023-04-01 18:18:45 -0400

    Changing project structure

--- Iteration3/src/com.termproject/Main.java
+++ Iteration3/src/com.termproject/Main.java
@@ -1,17 +1,18 @@
 package com.termproject;
 
 public class Main {
-    private RWStrategy DataIO;
     private Trip ActiveTrip;
     private PackageList Packages;
     private PersonList People;
     private AgentList Agents;
-    private static void LoadConfigFile() {
 
+    private static String LoadConfigFile() {
+        System.out.println("Loading config...");
+        return "json";
     }
 
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        String format = LoadConfigFile();
+        System.out.println("Loaded config: " + format);
     }
 }

56e87c6bcd9294694081dd8c01bcccdb891e187e
phredmund <eexley@gmail.com>
2023-04-07 15:04:19 -0400

    Adding comments and Doxygen tags

--- Iteration3/term_project/src/main/java/com/termproject/Trip/Package.java
+++ Iteration3/term_project/src/main/java/com/termproject/Trip/Package.java
@@ -2,10 +2,99 @@ package com.termproject.Trip;
 
 import com.termproject.Transport.TransportType;
 
+/**
+ * @file Package.java
+ *
+ * Represents a travel package that includes a price, hours of travel time, origin and destination Places,
+ * and the mode of transport.
+ */
+
 public class Package {
-    public float price;
-    public int hoursOfTravelTime;
-    public Place travelsFrom;
-    public Place travelsTo;
-    public TransportType transport;
+    /**
+     * The price of the travel package.
+     */
+    private final float price;
+
+    /**
+     * The number of hours it takes to travel from the origin Place to the destination Place.
+     */
+    private final int hoursOfTravelTime;
+
+    /**
+     * The Place where the travel package originates from.
+     */
+    private final Place travelsFrom;
+
+    /**
+     * The Place where the travel package is going to.
+     */
+    private final Place travelsTo;
+
+    /**
+     * The mode of transport for the travel package.
+     */
+    private final TransportType transport;
+
+    /**
+     * Creates a new travel package with the specified price, travel time, origin and destination Places,
+     * and mode of transport.
+     *
+     * @param price The price of the travel package.
+     * @param hoursOfTravelTime The number of hours it takes to travel from the origin Place to the destination Place.
+     * @param travelsFrom The Place where the travel package originates from.
+     * @param travelsTo The Place where the travel package is going to.
+     * @param transport The mode of transport for the travel package.
+     */
+    public Package(float price, int hoursOfTravelTime, Place travelsFrom, Place travelsTo, TransportType transport) {
+        this.price = price;
+        this.hoursOfTravelTime = hoursOfTravelTime;
+        this.travelsFrom = travelsFrom;
+        this.travelsTo = travelsTo;
+        this.transport = transport;
+    }
+
+    /**
+     * Returns the price of the travel package.
+     *
+     * @return The price of the travel package.
+     */
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * Returns the number of hours it takes to travel from the origin Place to the destination Place.
+     *
+     * @return The number of hours it takes to travel from the origin Place to the destination Place.
+     */
+    public int getHoursOfTravelTime() {
+        return hoursOfTravelTime;
+    }
+
+    /**
+     * Returns the Place where the travel package originates from.
+     *
+     * @return The Place where the travel package originates from.
+     */
+    public Place getTravelsFrom() {
+        return travelsFrom;
+    }
+
+    /**
+     * Returns the Place where the travel package is going to.
+     *
+     * @return The Place where the travel package is going to.
+     */
+    public Place getTravelsTo() {
+        return travelsTo;
+    }
+
+    /**
+     * Returns the mode of transport for the travel package.
+     *
+     * @return The mode of transport for the travel package.
+     */
+    public TransportType getTransport() {
+        return transport;
+    }
 }

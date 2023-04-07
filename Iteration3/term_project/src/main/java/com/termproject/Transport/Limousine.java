04d1b43c83ce32c655140109806e50afa63d4d01
phredmund <eexley@gmail.com>
2023-04-07 15:03:09 -0400

    Fleshing out transport type classes

--- Iteration3/term_project/src/main/java/com/termproject/Transport/Limousine.java
+++ Iteration3/term_project/src/main/java/com/termproject/Transport/Limousine.java
@@ -1,4 +1,20 @@
 package com.termproject.Transport;
 
+import com.termproject.Trip.Place;
+
 public class Limousine extends TransportType{
+    /**
+     * Constructor for the TransportType class.
+     *
+     * @param arrivalTime The arrival time of the transport.
+     * @param arrivalDate The arrival date of the transport.
+     * @param destination The destination of the transport.
+     * @param departTime  The departure time of the transport.
+     * @param price       The price of the transport.
+     * @param departFrom  The place the transport departs from.
+     * @param departDate  The departure date of the transport.
+     */
+    public Limousine(String arrivalTime, String arrivalDate, Place destination, String departTime, float price, Place departFrom, String departDate) {
+        super(arrivalTime, arrivalDate, destination, departTime, price, departFrom, departDate);
+    }
 }

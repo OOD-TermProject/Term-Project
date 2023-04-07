5a4cb9f412da5325c5f83f42b748c3932615f7c0
phredmund <eexley@gmail.com>
2023-04-07 15:05:07 -0400

    Adding comments, Doxygen tags, and getter

--- Iteration3/term_project/src/main/java/com/termproject/Trip/Place.java
+++ Iteration3/term_project/src/main/java/com/termproject/Trip/Place.java
@@ -1,5 +1,28 @@
 package com.termproject.Trip;
 
+/**
+ * @file Place.java
+ *
+ * Represents a place with a name.
+ */
 public class Place {
-    public String Name;
+    private final String name;
+
+    /**
+     * Constructs a new Place object with the given name.
+     *
+     * @param name the name of the place
+     */
+    public Place(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the name of this place.
+     *
+     * @return the name of this place
+     */
+    public String getName() {
+        return name;
+    }
 }

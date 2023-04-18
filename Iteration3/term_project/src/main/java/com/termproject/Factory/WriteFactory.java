808350a03a60c74815d18390764c80fb6a80e326
phredmund <eexley@gmail.com>
2023-04-17 22:52:04 -0400

    Updating factory classes to match new architecture

--- Iteration3/term_project/src/main/java/com/termproject/Factory/WriteFactory.java
+++ Iteration3/term_project/src/main/java/com/termproject/Factory/WriteFactory.java
@@ -1,5 +1,18 @@
 package com.termproject.Factory;
 
+import com.termproject.Strategy.JSONStrategy;
+import com.termproject.Strategy.RWStrategy;
+import com.termproject.Strategy.XMLStrategy;
+
 public class WriteFactory {
 
+    public RWStrategy createStrategy(String dataFormat) {
+        if (dataFormat.equalsIgnoreCase("json")) {
+            return new JSONStrategy();
+        } else if (dataFormat.equalsIgnoreCase("xml")) {
+            return new XMLStrategy();
+        } else {
+            return null;
+        }
+    }
 }

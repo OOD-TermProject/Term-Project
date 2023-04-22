a8671a98b9d74f6def04677134db9809a44d801e
phredmund <eexley@gmail.com>
2023-04-22 16:12:55 -0500

    Formatting

--- Iteration3/term_project/src/main/java/com/termproject/State/AwaitTravelersState.java
+++ Iteration3/term_project/src/main/java/com/termproject/State/AwaitTravelersState.java
@@ -13,36 +13,26 @@ public class AwaitTravelersState implements State {
 	private List<Traveler> travelers;
 	
 	public AwaitTravelersState() {
-		
 		travelers = new ArrayList<>();
-		
 	}
 	
 	public void addTraveler(Traveler traveler) {
-		
 		travelers.add(traveler);
-		
 	}
 
 	@Override
 	public void save() {		//Write to disk?
-		
 		//TODO
-		
 	}
 
 	@Override
 	public State resume() {
-		
 		return this;
-		
 	}
 
 	@Override
 	public State advanceState() {
-
 		return new AwaitPackagesState();
-		
 	}
 
 	@Override

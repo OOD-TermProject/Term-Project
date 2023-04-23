06991bb649975614d835e319574ca2b4fdc31287
phredmund <eexley@gmail.com>
2023-04-23 18:00:58 -0500

    Fleshing out class

--- Iteration3/term_project/src/main/java/com/termproject/Singleton/PersonList.java
+++ Iteration3/term_project/src/main/java/com/termproject/Singleton/PersonList.java
@@ -1,10 +1,35 @@
 package com.termproject.Singleton;
 
+import com.termproject.People.Customer;
+import com.termproject.People.Traveler;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class PersonList {
     private static PersonList uniqueInstance = new PersonList();
+    private static final String peopleFile = "term_project/src/main/java/com/termproject/Singleton/people.txt";
+    ArrayList<Customer> personList = new ArrayList<>();
 
     private PersonList() {
-        // Load the person list in here
+        loadPersonFile();
+    }
+
+    private void loadPersonFile() {
+        try (BufferedReader br = new BufferedReader(new FileReader(peopleFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                String name = data[0];
+                String mobilePhone = data[1];
+                String favoriteDrink = data[2];
+                personList.add(new Traveler(name, mobilePhone, favoriteDrink));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static PersonList getInstance() {

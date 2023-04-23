13cf5771d5539615f71ae77a06827a3f21e2d6d5
phredmund <eexley@gmail.com>
2023-04-22 20:00:41 -0400

    [WIP] Beginning implemetation of XML read/write

--- Iteration3/term_project/src/main/java/com/termproject/Strategy/XMLStrategy.java
+++ Iteration3/term_project/src/main/java/com/termproject/Strategy/XMLStrategy.java
@@ -6,36 +6,52 @@ import java.util.ArrayList;
 
 public class XMLStrategy extends RWStrategy {
 
-    /**
-     * @param tripID
+    private static final String filePath = "term_project/src/main/java/com/termproject/trips.xml";
+    private static ArrayList<Trip> tripList;
+
+    /** Saves trip to disk
+     * @param tripToSave
      */
     @Override
-    void saveTrip(int tripID) {
+    public void saveTrip(Trip tripToSave) {
 
     }
 
-    /**
+    /** Loads a specific trip for the given ID
      * @param tripID
-     * @return
+     * @return Trip object if the trip with matching ID was found. Else, null
      */
     @Override
     public Trip loadTrip(int tripID) {
+        System.out.println("[XMLStrategy] Attempting to load trip #" + tripID);
+        for (Trip trip : tripList) {
+            if (trip.getUniqueId() == tripID) {
+                return trip;
+            }
+        }
         return null;
     }
 
-    /**
-     * @return
+    /** Loads all trips from the XML file
+     * @return ArrayList of all trips read in
      */
     @Override
     public ArrayList<Trip> getAllTrips() {
         return null;
     }
 
-    /**
-     * @return
+    /** Get the highest numbered trip that we know about
+     * @return int Maximum trip ID
      */
     @Override
     public int getMaxTripID() {
-        return 0;
+        int x = 0;
+        for (Trip trip : tripList) {
+            int thisTripID = trip.getUniqueId();
+            if (thisTripID > x) {
+                x = thisTripID;
+            }
+        }
+        return x;
     }
 }

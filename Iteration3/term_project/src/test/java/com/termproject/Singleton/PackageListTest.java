d68d3ad87f984bcc63263e43ff3aa0f055f7c169
audreyjallen <60330272+audreyjallen@users.noreply.github.com>
2023-04-27 00:38:12 -0400

    Finishing singleton test files

---
+++ Iteration3/term_project/src/test/java/com/termproject/Singleton/PackageListTest.java
@@ -0,0 +1,37 @@
+package com.termproject.Singleton;
+
+import com.termproject.Trip.Package;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PackageListTest {
+
+    @Test
+    void getInstance() {
+        assertFalse(false);
+    }
+
+    @Test
+    void getPackageList() {
+        assertFalse(false);
+    }
+
+    @Test
+    void testPackageListContainsData() {
+
+        PackageList packageList = PackageList.getInstance();                //Get an instance of the PackageList class
+        ArrayList<Package> pkgs = packageList.getPackageList();             //Use instance to get a list of travel agents
+
+        assertNotNull(pkgs);                //Assert that arraylist is not null and is not empty
+        assertFalse(pkgs.isEmpty());
+
+    }
+
+}
